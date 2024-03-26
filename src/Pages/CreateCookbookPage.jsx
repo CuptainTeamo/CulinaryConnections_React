efe76ab95280003a698bf52baa7089798a90d00f
Frank Carusi <frankcarusi.fc@gmail.com>
2024-03-25 23:20:54 -0400

    CreateCookbookCard component, page, and styling complete. HomePage and styling complete.

---
+++ src/Pages/CreateCookbookPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import CreateCookbookCard from "../Components/CreateCookbookCard/CreateCookbookCard.jsx";
+
+
+function CreateCookbookPage(){
+    return(
+        <div className="body">
+            <CreateCookbookCard />
+        </div>
+    );
+}
+
+export default CreateCookbookPage;
